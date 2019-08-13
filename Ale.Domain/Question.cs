782ed49481f91a237437a11fe1d72f637eac9e7d
Raudel Ravelo Suarez <raudelravelo91@gmail.com>
2019-08-12 23:54:54 -0400

    Add Default Questions controller.

--- Ale.Domain/Question.cs
+++ Ale.Domain/Question.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,11 @@ namespace Ale.Data
 {
     public class Question
     {
+        [Key]
         public int Id { get; set; }
 
         public int Text { get; set; }
+
+        public List<string> PossibleAnswers { get; set; }
     }
 }
